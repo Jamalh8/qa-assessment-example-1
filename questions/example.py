15ac9305ff8b750228d41b1152ca7f19f1b085a9
jamalh8 <jamalh.888@googlemail.com>
2022-05-20 10:04:13 +0000

    added function

--- questions/example.py
+++ questions/example.py
@@ -11,7 +11,12 @@
 
 	# <HINT>
 
-	# What was the name of the function we have seen which changes the case of a string?  Use your CLI to access the Python documentation and get help(str).
+	# What was the name of the function we have seen which changes the case of a string?  
+	# Use your CLI to access the Python documentation and get help(str).
     
 def endsPy(input):
-	return False
+	input_lower = input.lower()
+	if input_lower.endswith('py'):
+		return True
+	else:
+		return False 
